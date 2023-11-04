1f239c4e141761a1388c6fd504d74f1bd82a7226
sabarish-learning <66541812+sabarish-learning@users.noreply.github.com>
2023-11-04 18:29:10 +0530

    Update build_and_push.sh

--- script/build_and_push.sh
+++ script/build_and_push.sh
@@ -5,23 +5,23 @@ IMAGE_NAME_PROD="sabarish24/react-prod"
 IMAGE_NAME_DEV="sabarish24/react-dev"
 IMAGE_TAG="1.0"
 
-currentbranch=$(git rev-parse --abbrev-ref HEAD)
-                    
-                    if [ currentBranch == 'dev' ]; then
-                        // Build and push the development image
-                      docker build -t $IMAGE_NAME_DEV:$IMAGE_TAG -f ./Dockerfile.dev .
-                         echo "$LOGIN_CREDS_PSW" | docker login -u "$LOGIN_CREDS_USR" --password-stdin
-                         docker push $IMAGE_NAME_DEV:$IMAGE_TAG
-          
-                        echo "Docker image has been built and pushed to Dev Docker Hub."
-                
-                    elif [ currentBranch == 'master' ]; then 
-                        // Build and push the production image
-                       docker build -t $IMAGE_NAME_PROD:$IMAGE_TAG -f ./Dockerfile.prod .
-                        echo "$LOGIN_CREDS_PSW" | docker login -u "$LOGIN_CREDS_USR" --password-stdin
-                           docker push $IMAGE_NAME_PROD:$IMAGE_TAG
-                       echo "Docker image has been built and pushed to Prod Docker Hub."
-                
-                    else 
-                        echo "Code pushed to a branch that doesn't trigger image build."
-                    fi
+# Get the current branch name
+currentBranch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$currentBranch" == "dev" ]; then
+    # Build and push the development image
+    docker build -t "$IMAGE_NAME_DEV:$IMAGE_TAG" -f ./Dockerfile.dev .
+    echo "$LOGIN_CREDS_PSW" | docker login -u "$LOGIN_CREDS_USR" --password-stdin
+    docker push "$IMAGE_NAME_DEV:$IMAGE_TAG"
+    echo "Docker image has been built and pushed to Dev Docker Hub."
+
+elif [ "$currentBranch" == "master" ]; then
+    # Build and push the production image
+    docker build -t "$IMAGE_NAME_PROD:$IMAGE_TAG" -f ./Dockerfile.prod .
+    echo "$LOGIN_CREDS_PSW" | docker login -u "$LOGIN_CREDS_USR" --password-stdin
+    docker push "$IMAGE_NAME_PROD:$IMAGE_TAG"
+    echo "Docker image has been built and pushed to Prod Docker Hub."
+
+else
+    echo "Code pushed to a branch that doesn't trigger image build."
+fi
