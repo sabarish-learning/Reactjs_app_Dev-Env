382fd14a3b0cd82f461c12ec2c23cd08ebae6937
sabarish-learning <66541812+sabarish-learning@users.noreply.github.com>
2023-11-04 20:19:08 +0530

    Update build_prod.sh

--- script/build_prod.sh
+++ script/build_prod.sh
@@ -5,7 +5,7 @@ IMAGE_NAME_PROD="sabarish24/react-prod"
 IMAGE_NAME_DEV="sabarish24/react-dev"
 IMAGE_TAG="1.0"
 
-
+if []; then
     echo "Building and pushing the image to the prod repository on Docker Hub"
     
     # Build the Docker image
@@ -17,3 +17,5 @@ IMAGE_TAG="1.0"
     # Push the image to the prod repository
     docker push "$IMAGE_NAME_PROD:$IMAGE_TAG"
 
+else
+fi
