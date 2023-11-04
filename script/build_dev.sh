4829094c09cfc50429dd0189237f042936ee06c5
sabarish-learning <66541812+sabarish-learning@users.noreply.github.com>
2023-11-04 20:29:32 +0530

    Update build_dev.sh

--- script/build_dev.sh
+++ script/build_dev.sh
@@ -5,6 +5,7 @@ IMAGE_NAME_PROD="sabarish24/react-prod"
 IMAGE_NAME_DEV="sabarish24/react-dev"
 IMAGE_TAG="1.0"
 
+echo "started"
 if [ BRANCH_NAME == 'dev' ]; then
     # Triggered on code push to dev branch
     echo "Building and pushing the image to the dev repository on Docker Hub"
