551aaf26215a4493b0a497ca74cd1df8d1a29203
sabarish-learning <66541812+sabarish-learning@users.noreply.github.com>
2023-11-04 17:27:53 +0530

    Update build_and_push.sh

--- script/build_and_push.sh
+++ script/build_and_push.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Define your Docker image name and tag
@@ -10,17 +11,17 @@ IMAGE_TAG="1.0"
                     if (currentBranch == 'dev') {
                         // Build and push the development image
                       docker build -t $IMAGE_NAME_DEV:$IMAGE_TAG -f ./Dockerfile.dev .
-                        withDockerRegistry([credentialsId: '261b4bc0-b4a4-471f-a23c-0821e2dd462d', url: 'https://index.docker.io/v1/']) {
+                         echo "$LOGIN_CREDS_PSW" | docker login -u "$LOGIN_CREDS_USR" --password-stdin
                          docker push $IMAGE_NAME_DEV:$IMAGE_TAG
-                        }
+               
                         echo "Docker image has been built and pushed to Dev Docker Hub."
                     }
                     else if (currentBranch == 'master') {
                         // Build and push the production image
                        docker build -t $IMAGE_NAME_PROD:$IMAGE_TAG -f ./Dockerfile.prod .
-                        withDockerRegistry([credentialsId: '261b4bc0-b4a4-471f-a23c-0821e2dd462d', url: 'https://index.docker.io/v1/']) {
+                        echo "$LOGIN_CREDS_PSW" | docker login -u "$LOGIN_CREDS_USR" --password-stdin
                            docker push $IMAGE_NAME_PROD:$IMAGE_TAG
-                        }
+                       
                         echo "Docker image has been built and pushed to Prod Docker Hub."
                     }
                     else {
