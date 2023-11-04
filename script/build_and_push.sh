c3844ec6195bb89f3dd02d171ae0275492322ca6
sabarish-learning <66541812+sabarish-learning@users.noreply.github.com>
2023-11-04 17:45:44 +0530

    Update build_and_push.sh

--- script/build_and_push.sh
+++ script/build_and_push.sh
@@ -7,23 +7,21 @@ IMAGE_TAG="1.0"
 
 currentbranch=$(git rev-parse --abbrev-ref HEAD)
                     
-                    if (currentBranch == 'dev') {
+                    if [ currentBranch == 'dev' ]; then
                         // Build and push the development image
                       docker build -t $IMAGE_NAME_DEV:$IMAGE_TAG -f ./Dockerfile.dev .
                          echo "$LOGIN_CREDS_PSW" | docker login -u "$LOGIN_CREDS_USR" --password-stdin
                          docker push $IMAGE_NAME_DEV:$IMAGE_TAG
-                 }
-                    else {
+          
                         echo "Docker image has been built and pushed to Dev Docker Hub."
-                    }
-                    else if (currentBranch == 'master') {
+                
+                    elif [ currentBranch == 'master' ]; then 
                         // Build and push the production image
                        docker build -t $IMAGE_NAME_PROD:$IMAGE_TAG -f ./Dockerfile.prod .
                         echo "$LOGIN_CREDS_PSW" | docker login -u "$LOGIN_CREDS_USR" --password-stdin
                            docker push $IMAGE_NAME_PROD:$IMAGE_TAG
-                       
-                        echo "Docker image has been built and pushed to Prod Docker Hub."
-                    }
-                    else {
+                       echo "Docker image has been built and pushed to Prod Docker Hub."
+                
+                    else 
                         echo "Code pushed to a branch that doesn't trigger image build."
-                    }
+                    fi
